e203876cf6dee36d974e5bd7575f8e7125f75afc
Thiago Pereira Alves <tpereiraalves2013@outlook.com>
2025-08-24 20:32:02 -0300

    feat: adicionando criação de DNS e push para o ACR

--- environment.sh
+++ environment.sh
@@ -3,7 +3,10 @@ set -e
 
 ACR_NAME="acrestudos"
 AKS_NAME="aksestudos"
-RG_NAME="rg-estudos"
+APPGW_NAME="appgw-estudos"
+DNS_ZONE="acrestudos.com.br"
+RG_NAME="rg-estudos_dev"
+RG_NAME_AKS_ADDONS="MC_rg-estudos_dev_aksestudos_brazilsouth"
 LOCATION="brazilsouth"
 DESTROY_ENVIRONMENT=${1:-false}
 COMMAND_TAG='az tag create --resource-id /subscriptions/$code --tags estudos=devops Status=Normal'
@@ -70,16 +73,23 @@ echo "Realizando login no ACR '$ACR_NAME'..."
 az acr login --name $ACR_NAME
 
 echo "Criando o cluster AKS 'aks-estudos'..."
+
 az aks create \
   --resource-group $RG_NAME \
   --name $AKS_NAME \
   --node-count 1 \
-  --node-vm-size Standard_B2s \
-  --generate-ssh-keys \
   --enable-managed-identity \
-  --location $LOCATION \
+  --network-plugin kubenet \
   --kubernetes-version 1.30.14 \
-  --network-plugin kubenet
+  --dns-name-prefix $AKS_NAME \
+  --load-balancer-sku standard \
+  --network-policy none \
+  --enable-app-routing \
+  --appgw-name $APPGW_NAME \
+  --appgw-subnet-cidr "10.225.0.0/24" \
+  --enable-addons ingress-appgw \
+  --generate-ssh-keys \
+  --location $LOCATION
 
 if [ $? -eq 0 ]; then
   echo "Cluster AKS '$AKS_NAME' criado com sucesso."
@@ -90,6 +100,32 @@ fi
 
 echo "Aguardando o AKS '$AKS_NAME' ficar disponível..."
 sleep 60
+
+echo "Criando a zona DNS $DNS_ZONE"
+az network dns zone create --resource-group $RG_NAME_AKS_ADDONS --name $DNS_ZONE
+if [ $? -eq 0 ]; then
+  echo "Zona DNS '$DNS_ZONE' criada com sucesso."
+else
+  echo "Falha ao criar a zona DNS '$DNS_ZONE'."
+  exit 1
+fi
+
+echo "Configurando permissões do AKS para acessar o ACR e DNS..."
+APPGW_IDENTITY=$(az aks show \
+  --resource-group $RG_NAME \
+  --name $AKS_NAME \
+  --query "addonProfiles.ingressApplicationGateway.identity.objectId" \
+  -o tsv)
+
+echo "Identidade do Application Gateway: $APPGW_IDENTITY"
+
+DNS_ZONE_ID=$(az network dns zone show -g $RG_NAME_AKS_ADDONS -n $DNS_ZONE --query "id" -o tsv)
+
+echo "Atribuindo a função DNS Zone Contributor à identidade do Application Gateway..."
+az role assignment create \
+  --assignee $APPGW_IDENTITY \
+  --role "DNS Zone Contributor" \
+  --scope $DNS_ZONE_ID
 
 echo "Concedendo permissão ao AKS para acessar o ACR..."
 az aks update \
@@ -113,11 +149,20 @@ else
   exit 1
 fi
 
+echo "Realizando o push da imagem Docker para o ACR..."
+docker push acrestudos.azurecr.io/simple-add:1.0.1
+
+if [ $? -eq 0 ]; then
+  echo "Imagem Docker enviada com sucesso para o ACR."
+else
+  echo "Falha ao enviar a imagem Docker para o ACR."
+  exit 1
+fi
+
 echo "Adicionando tags aos recursos..."
-replace_subscription_id $RG_NAME
 replace_subscription_id $ACR_NAME
 replace_subscription_id $AKS_NAME
-
-
+replace_subscription_id $APPGW_NAME
+replace_subscription_id $DNS_ZONE
 
 echo "Ambiente criado com sucesso!"
